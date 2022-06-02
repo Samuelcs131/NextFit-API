a50154a539d1337a6c72149d0013b276ef08d6be
Samuel Claudino <samuelcs131@gmail.com>
2022-06-02 06:52:35 -0300

    ajuste de index rota principal

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,17 +3,14 @@ import * as userController from '@controllers/users'
 import * as trainingController from '@controllers/trainings'
 import * as measurementsController from '@controllers/measurements'
 import auth from '@controllers/auth'
-// import path from 'path'
+import path from 'path'
 
 // CONTROLLERS
 const routes = express.Router()
 
 // INDEX
-/* routes.get('/', (req: Request, res: Response) => {
-  res.sendFile(path.resolve(__dirname, '..', '..', 'public/page/index.html'))
-}) */
 routes.get('/', (req: Request, res: Response) => {
-  res.send('Opa ta funcionando em!')
+  res.sendFile(path.resolve(__dirname, '..', '..', 'public/page/index.html'))
 })
 
 console.log()
