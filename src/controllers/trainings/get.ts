cd03951fab3f2c41ed21aaafe8337c21294ff441
Samuel Claudino <samuelcs131@gmail.com>
2022-07-20 22:17:30 -0300

    ajuste rota trainings

--- src/controllers/trainings/get.ts
+++ src/controllers/trainings/get.ts
@@ -56,17 +56,31 @@ export const findTrainingsByIdUser = async (req: Request, res: Response) => {
 
     try {
       // SEARCH USERS
-      const training = await prisma.training.findMany({
+      const trainings = await prisma.training.findMany({
         where: { userId: idUser }
       })
+      const exercises = await prisma.exercises.findMany()
+
+      const muscles = await prisma.muscles.findMany()
 
       // RETURN
       status200('Pesquisa realizada!')
-      res.status(200).send(training)
+      res.status(200).send(trainings.map((training) => {
+        return ({
+          id: training.id,
+          exercise: exercises.find((exercise) => exercise.id === training.exercisesId)?.name,
+          muscle: muscles.find((muscle) => muscle.id === exercises.find((exercise) => exercise.id === training.exercisesId)?.muscleId)?.name,
+          date: training.date,
+          series: training.series,
+          repetitions: training.repetitions,
+          weight: training.weight,
+          interval: training.interval,
+          createAt: training.createAt
+        })
+      }))
 
       // ERROR!
     } catch (error) {
-      console.log(error)
       return res.status(400).send(status400('O ID fornecido é invalido!'))
     }
 
