8c7f378c97846b345f0cbb8f1456722460a18892
Samuel <samuelcs131@gmail.com>
2022-11-24 02:47:38 -0300

    add components in swaggerFile

--- src/services/swagger/index.ts
+++ src/services/swagger/index.ts
@@ -1,7 +1,7 @@
 import config from '@services/swagger/json/config.json'
+import components from '@services/swagger/json/components.json'
 import users from '@services/swagger/json/users.json'
 import exercises from '@services/swagger/json/exercises.json'
-import components from '@services/swagger/json/components.json'
 
 export const swaggerFile = {
   ...config,
