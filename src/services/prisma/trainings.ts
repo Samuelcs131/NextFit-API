09614f74485ce8da9cc2e6c19ad003a950d35aab
Samuel <samuelcs131@gmail.com>
2022-11-26 15:17:09 -0300

    add service training

---
+++ src/services/prisma/trainings.ts
@@ -0,0 +1,56 @@
+import { Training, Prisma, PrismaClient } from '@prisma/client'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
+import { handleApiError, handleApiResult } from '@services/handleApi'
+import { IApiResponse } from 'src/@types/service/IApiResponse.type'
+
+const { training } = new PrismaClient()
+
+export async function update (args: Prisma.TrainingUpdateArgs): Promise<IApiResponse<Training>> {
+  try {
+    const trainingsPromisse = await training.update(args)
+
+    return handleApiResult(trainingsPromisse)
+  } catch (error) {
+    return handleApiError(error as Error & PrismaClientKnownRequestError)
+  }
+}
+
+export async function create (args: Prisma.TrainingCreateArgs): Promise<IApiResponse<Training>> {
+  try {
+    const trainingsPromisse = await training.create(args)
+
+    return handleApiResult(trainingsPromisse)
+  } catch (error) {
+    return handleApiError(error as Error & PrismaClientKnownRequestError)
+  }
+}
+
+export async function findMany (args?: Prisma.TrainingFindManyArgs): Promise<IApiResponse<Training[]>> {
+  try {
+    const trainingsPromisse = await training.findMany(args)
+
+    return handleApiResult(trainingsPromisse)
+  } catch (error) {
+    return handleApiError(error as Error & PrismaClientKnownRequestError)
+  }
+}
+
+export async function findUnique (args: Prisma.TrainingFindUniqueArgs): Promise<IApiResponse<Training | null>> {
+  try {
+    const trainingsPromisse = await training.findUnique(args)
+
+    return handleApiResult(trainingsPromisse)
+  } catch (error) {
+    return handleApiError(error as Error & PrismaClientKnownRequestError)
+  }
+}
+
+export async function exclude (args: Prisma.TrainingDeleteArgs): Promise<IApiResponse<Training>> {
+  try {
+    const trainingsPromisse = await training.delete(args)
+
+    return handleApiResult(trainingsPromisse)
+  } catch (error) {
+    return handleApiError(error as Error & PrismaClientKnownRequestError)
+  }
+}
