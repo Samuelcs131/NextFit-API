58c2a7839b3ad4900ade27d880785f246969302e
Samuel <samuelcs131@gmail.com>
2022-11-27 14:08:16 -0300

    refactor route auth

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,67 +1,49 @@
 import { Request, Response } from 'express'
-import { PrismaClient, User } from '@prisma/client'
 import { compare } from 'bcrypt'
-import { status200, status400, status500 } from './response/status'
-import { generateTokenUser } from './token/generateToken'
+import { generateTokenUser } from '../utils/token/generateToken'
+import { verifyEmail, verifyString } from '@utils/verifications/valid'
+import { statusCode } from '@utils/status'
+import * as UsersService from '@services/prisma/users'
 
-const prisma = new PrismaClient()
-
-// POST
 const auth = async (req: Request, res: Response) => {
-  try {
-    // PARAMS
-    const email: string = req.body.email || req.body.body.email
-    const password: string = req.body.password || req.body.body.password
-    const inputs = [email, password]
+  const email: string = req.body.email
+  const password: string = req.body.password
 
-    // VERIFY INPUTS
-    for (let num = 0; num < inputs.length; num++) {
-      if (inputs[num] === null || inputs[num] === undefined || String(inputs[num]).trim() === '') {
-        return res.status(400).send(status400('Preencha todos os campos!'))
-      }
-    }
-    // eslint-disable-next-line
-    if (email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) === null) {
-      return res.status(400).send(status400('Email invalido!'))
-    }
-
-    // SEARCH USERS
-    const userData: User | null = await prisma.user.findUnique({
-      where: { email: String(email) }
-    })
-
-    if (userData === null || userData === undefined) {
-      return res.status(400).send(status400('Usuário ou senha incorretos!'))
-    }
-
-    // COMPARE
-    const comparePassword = await compare(password, userData?.password)
-    if (comparePassword === true) {
-      // RETURN
-      status200('Usuário autenticado!')
-      res.status(200).send(
-        {
-          user: {
-            id: userData.id,
-            name: userData.name,
-            lastName: userData.lastName,
-            email: userData.email,
-            height: userData.height,
-            weight: userData.weight,
-            sex: userData.sex
-          },
-          token: await generateTokenUser(userData.id)
-        })
-    } else {
-      // RETURN
-      status400('Senha não autenticado!')
-      res.status(400).send(status400('Usuário ou senha incorretos!'))
-    }
-
-    // ERROR!
-  } catch (error) {
-    res.status(500).send(status500(error))
+  if (
+    verifyString([password]) ||
+      verifyEmail(email)
+  ) {
+    return res.status(400).send(statusCode({ status: 400 }))
   }
+
+  const [userError, user] = await UsersService.findUnique({
+    where: { email: email.trim() }
+  })
+
+  if (userError || user === null) {
+    return res.status(404).send(statusCode({ status: 404, error: userError?.meta?.message }))
+  }
+
+  const comparePassword = await compare(password, user.password)
+
+  if (comparePassword === false) {
+    return res.status(401).send(statusCode({ status: 401 }))
+  }
+
+  const formated = {
+    user: {
+      id: user.id,
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      height: user.height,
+      weight: user.weight,
+      sex: user.sex
+    },
+    token: await generateTokenUser(user.id)
+  }
+
+  return res.status(200).send(formated)
 }
 
 export default auth
