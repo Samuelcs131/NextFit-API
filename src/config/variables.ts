4a51e27e9c40fa47578bdfe5c2a321a1802d3660
Samuel <samuelcs131@gmail.com>
2022-11-27 16:46:01 -0300

    add array authorized customers

--- src/config/variables.ts
+++ src/config/variables.ts
@@ -1,10 +1,12 @@
+import authorizedCustomers from './authorizedCustomers'
+
 const env = {
   production: {
-    port: process.env.PORT,
-    database: process.env.DATABASE_URL,
-    secret: process.env.SECRET,
-    sendGrid: process.env.SENDGRID_API_KEY,
-    authorization: process.env.NEXTFIT_API_KEY
+    port: process.env.PORT as string,
+    database: process.env.DATABASE_URL as string,
+    secret: process.env.SECRET as string,
+    sendGrid: process.env.SENDGRID_API_KEY as string,
+    authorization: authorizedCustomers
   }
 }
 
