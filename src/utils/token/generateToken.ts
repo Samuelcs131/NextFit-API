23ebe1c671a71c6e5a8d7ea542683e0a7f53afa7
Samuel <samuelcs131@gmail.com>
2022-11-27 14:09:33 -0300

    fix name parameter

--- src/utils/token/generateToken.ts
+++ src/utils/token/generateToken.ts
@@ -1,6 +1,6 @@
 import secret from '@config/secret'
 import { sign } from 'jsonwebtoken'
 
-export const generateTokenUser = async (idUser: string) => {
-  return sign({ id: idUser }, secret, { expiresIn: 86400 })
+export const generateTokenUser = async (userId: string) => {
+  return sign({ id: userId }, secret, { expiresIn: 86400 })
 }
