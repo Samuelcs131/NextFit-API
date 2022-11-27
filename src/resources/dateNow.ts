3e6e90824d48f6b7cd36878f7d3bff8839488959
Samuel <samuelcs131@gmail.com>
2022-11-27 14:15:25 -0300

    delete dateNow

--- src/resources/dateNow.ts
+++
@@ -1,6 +0,0 @@
-const dateBrazil: string = Intl.DateTimeFormat('pt-BR', { dateStyle: 'short' }).format().split('/').reverse().join('-')
-const hoursBrazil: string = Intl.DateTimeFormat('pt-BR', { timeStyle: 'medium' }).format()
-const dateNow: Date = new Date(dateBrazil + 'T' + hoursBrazil)
-dateNow.setHours(dateNow.getHours() - 3)
-
-export default dateNow
