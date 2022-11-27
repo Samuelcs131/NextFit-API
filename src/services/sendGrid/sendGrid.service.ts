9f4e6b39c3a28338326b4c52f041f1ff4adc88ac
Samuel <samuelcs131@gmail.com>
2022-11-27 14:07:11 -0300

    create service sendGrid

---
+++ src/services/sendGrid/sendGrid.service.ts
@@ -0,0 +1,21 @@
+import sgMail, { ClientResponse, MailDataRequired, ResponseError } from '@sendgrid/mail'
+import { handleApiError, handleApiResult } from '@services/handleApi'
+import { IApiResponse } from 'src/@types/service/IApiResponse.type'
+
+export async function setApiKey (apiKey: string): Promise<IApiResponse<void>> {
+  try {
+    const responseApi = await sgMail.setApiKey(apiKey)
+    return handleApiResult(responseApi)
+  } catch (error) {
+    return handleApiError(error as Error)
+  }
+}
+
+export async function send (data: MailDataRequired | MailDataRequired[], isMultiple?: boolean, cb?: (err: Error | ResponseError, result: [ClientResponse, {}]) => void): Promise<IApiResponse<[ClientResponse, {}]>> {
+  try {
+    const responseApi = await sgMail.send(data, isMultiple, cb)
+    return handleApiResult(responseApi)
+  } catch (error) {
+    return handleApiError(error as Error)
+  }
+}
