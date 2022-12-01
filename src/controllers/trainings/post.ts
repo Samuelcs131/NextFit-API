a0c0de95274b2cc4428e7e624ec626c511f27c88
Samuel <samuelcs131@gmail.com>
2022-11-30 22:04:27 -0300

    change repetitions to number

--- src/controllers/trainings/post.ts
+++ src/controllers/trainings/post.ts
@@ -1,31 +1,27 @@
 import { Request, Response } from 'express'
 import { Training } from '@prisma/client'
 import { $date, reverseDateFormat } from '@utils/date/date-functions'
-import { verifyArray, verifyNumber, verifyString } from 'src/validators/valid'
+import { verifyNumber, verifyString } from 'src/validators/valid'
 import { statusCode } from '@utils/status'
-import * as TrainingsService from '@services/prisma/trainings.service'
+import * as TrainingsService from '@services/prisma/training.service'
 
 export const createTraining = async (req: Request, res: Response) => {
   const userId: string = req.params.id
-  const { exercisesId, weight, series, interval }: Training = req.body
-  const repetitions: Array<number> = req.body.repetitions
+  const { exerciseId, weight, series, interval, repetitions }: Training = req.body
   const date: string = $date(reverseDateFormat(req.body.date || ''), true).format()
 
   if (
-    verifyString([exercisesId]) ||
-    verifyNumber([weight, series, interval]) ||
+    verifyString([exerciseId]) ||
+    verifyNumber([weight, series, interval, repetitions]) ||
     !$date(date, true).isValid() ||
-    verifyArray(repetitions, 'number') ||
-    repetitions.length !== series ||
-    repetitions.length > 10 ||
-    repetitions.some((value: number) => value > 99)
+    repetitions > 99
   ) {
     return res.status(400).send(statusCode({ status: 400 }))
   }
 
   const args = {
     data: {
-      exercisesId: exercisesId.trim(),
+      exerciseId: exerciseId.trim(),
       userId: userId.trim(),
       weight,
       repetitions,
@@ -36,11 +32,11 @@ export const createTraining = async (req: Request, res: Response) => {
     }
   }
 
-  const [error] = await TrainingsService.create(args)
+  const [error, training] = await TrainingsService.create(args)
 
   if (error) {
     return res.status(422).send(statusCode({ status: 422, error: error.meta?.message }))
   }
 
-  res.status(204)
+  return res.status(200).send(training)
 }
